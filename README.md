bdb8d6d0c3f3aeada1a589b2356de42e93b37e3e
yazzy14 <yazminbazail@icloud.com>
2022-10-18 00:15:20 -0700

    Ready to turn in

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# README-Generator
+# README Generator
+
+## Table of Contents
+* [Description](##Description) 
+* [Installation](##Installation) 
+* [Usage](##Usage) 
+* [Contribution](##Contribution) 
+* [Test](##tests) 
+* [Credits](##Credits) 
+* [License](##License) 
+* [Questions](##Questions)
+
+## Description
+* My motivation was to create a project that can create a professional readme.
+* I built this project to easily and quickly create readme files.
+* I always look up readme templates for my projects/homework this project allows me  to have a template I created.
+* I learned that node.js can be helpful for templates and possibly other bigger scale  projects.
+
+## Installation
+  In command line type
+
+       npm init -y
+----------------------------------------------------------------------
+       npm i inquirer@8.2.4
+
+ ## Usage
+Inquirery is used with the npm install
+
+## Contribution
+ Public repository feel free to add suggestions
+
+## Tests
+In the command lien you can run:
+
+       node index.js
+
+## Helpful Links 
+- [NPM-Installation](https://www.npmjs.com/package/inquirer)
+- [Fs](https://www.w3schools.com/nodejs/nodejs_filesystem.asp)
+
+## Credits
+- UW Bootcamp starter code 'bonus.js' file I used ".then((answers) => writeFile('Index.html', generateHTML(answers)))" to write my init function .
+
+## License
+The license used for this project is MIT looking in repository for more details.
+
+## Questions
+- Github: [ybazail3](https://github.com/ybazail3)
+- Email: yazminbazail@icloud.com
+       
--- index.js
+++ index.js
@@ -7,140 +7,136 @@ const { writeFile } = require('fs').promises
 
 // TODO: Create an array of questions for user input
 const genUser = () => {
-return inquirer.prompt([
-    {
-        type: 'input',
-        message: 'Write the title of your project',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What was your motivation?',
-        name: 'descriptionpart1',
-    },
-    {
-        type: 'input',
-        message: 'Why did you build this project?',
-        name: 'descriptionpart2',
-    },
-    {
-        type: 'input',
-        message: 'What problem does it solve?',
-        name: 'descriptionpart3',
-    },
-    {
-        type: 'input',
-        message: 'What did you learn?',
-        name: 'descriptionpart4',
-    },
-    {
-        type: 'input',
-        message: 'What makes your project stand out?',
-        name: 'descriptionpart5',
-    },
-    {
-        type: 'input',
-        message: 'Add table of contents (optional)',
-        name: 'tableofcontents',
-    },
-    {
-        type: 'input',
-        message: 'Add instructions for installation',
-        name: 'installation',
-    },
-    {
-        type: 'input',
-        message: 'Add usage',
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'Add contributors',
-        name: 'contribution',
-    },
-    {
-        type: 'input',
-        message: 'How do we test?',
-        name: 'tests',
-    },
-    {
-        type: 'input',
-        message: 'Add credits list any colllaborators with links to their GitHub profiles.',
-        name: 'credits',
-    },
-    {
-        type: 'list',
-        message: 'Add license (Use arrow keys)',
-        name: 'license',
-        choices: ['MIT', 'Apache', 'GPLv2', 'Unlicense'],
-    },
-    {
-        type: 'input',
-        message: 'Enter GitHub username ',
-        name: 'github',
-    },
-    {
-        type: 'input',
-        message: 'Enter you e-mail address',
-        name: 'email',
-    },
-])
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Write the title of your project',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What was your motivation?',
+            name: 'descriptionpart1',
+        },
+        {
+            type: 'input',
+            message: 'Why did you build this project?',
+            name: 'descriptionpart2',
+        },
+        {
+            type: 'input',
+            message: 'What problem does it solve?',
+            name: 'descriptionpart3',
+        },
+        {
+            type: 'input',
+            message: 'What did you learn?',
+            name: 'descriptionpart4',
+        },
+        {
+            type: 'input',
+            message: 'Add table of contents (optional)',
+            name: 'tableofcontents',
+        },
+        {
+            type: 'input',
+            message: 'Add instructions for installation',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Add usage',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Add contributors',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'How do we test?',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'Add credits list any colllaborators with links to their GitHub profiles.',
+            name: 'credits',
+        },
+        {
+            type: 'list',
+            message: 'Add license (Use arrow keys)',
+            name: 'license',
+            choices: ['MIT', 'Apache', 'GPLv2', 'Unlicense'],
+        },
+        {
+            type: 'input',
+            message: 'Enter GitHub username ',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'Enter you e-mail address',
+            name: 'email',
+        },
+    ])
 };
-    // .then((answers) => {
-    //     const readMe = createReadMe(answers);
-
-    //     fs.writeFile('README.md', readMe, (err) =>
-    //         err ? console.log(err) : console.log('Created README!')
-    //     );
-    // });
 
 // TODO: Create a function to write README file
-//  function writeToFile() {
-    const createReadMe = ({
-        title,
-        descriptionpart1,
-        descriptionpart2,
-        descriptionpart3,
-        descriptionpart4,
-        descriptionpart5,
-        tableofcontents,
-        installation,
-        credits,
-        license,
-        github,
-        email }) =>
-        `# ${title}
-       # Description
+const createReadMe = ({
+    title,
+    descriptionpart1,
+    descriptionpart2,
+    descriptionpart3,
+    descriptionpart4,
+    tableofcontents,
+    installation,
+    usage,
+    contribution,
+    tests,
+    credits,
+    license,
+    github,
+    email }) =>
+
+    `# ${title}
+       
+       ## Table of Contents
+       - ${tableofcontents}
+
+       ## Description
        * ${descriptionpart1}
        * ${descriptionpart2}
        * ${descriptionpart3}
        * ${descriptionpart4}
-       * ${descriptionpart5}
-       # Table of Contents
-       ${tableofcontents}
-       # Installation
+
+       ## Installation
         ${installation}
-        # Usage
+
+        ## Usage
         ${usage}
-        # Contribution
+
+        ## Contribution
         ${contribution}
-        # Tests
+
+        ## Tests
         ${tests}
-        # Credits
+
+        ## Credits
         - ${credits}
-       #License
-       The license used for this project is ${license}
-       # Questions
+
+       ## License
+       The license used for this project is: ${license}
+
+       ## Questions
        Github: ${github}
        Email: ${email}
        `;
-    // };
 
 // // TODO: Create a function to initialize app
 function init() {
     genUser()
-
-    .then((answers) => writeFile('README.md', createReadMe(answers)))
+        .then((answers) => writeFile('README.md', createReadMe(answers)))
 }
 
 // Function call to initialize app
